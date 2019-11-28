aaed4f243e9fb31848d3c47b98b04d4a65f16a92
Nic Jackson <jackson.nic@gmail.com>
2019-11-28 20:17:38 +0000

    Fix bugs

--- clients/docker.go
+++ clients/docker.go
@@ -2,17 +2,17 @@ package clients
 
 import (
 	"context"
+
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/network"
-	"github.com/docker/docker/api/types/versions"
 )
 
 type Docker interface {
 	ContainerCreate(
-		ctx context.Context, 
-		config *container.Config, 
-		hostConfig *container.HostConfig, 
-		networkingConfig *network.NetworkingConfig, 
+		ctx context.Context,
+		config *container.Config,
+		hostConfig *container.HostConfig,
+		networkingConfig *network.NetworkingConfig,
 		containerName string,
 	) (container.ContainerCreateCreatedBody, error)
 }
--- providers/container.go
+++ providers/container.go
@@ -1,5 +1,9 @@
 package providers
 
+import (
+	"github.com/shipyard-run/cli/clients"
+)
+
 type Container struct {
 	client clients.Docker
 }
