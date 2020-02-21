5d50f9bc7aee2961d6fa18142c07628b1d42bb98
Nic Jackson <jackson.nic@gmail.com>
2020-02-21 08:37:11 +0000

    Start adding tests for shell command

---
+++ pkg/clients/docker_tasks_command_shell_test.go
@@ -0,0 +1,45 @@
+package clients
+
+import (
+	"bufio"
+	"bytes"
+	"io/ioutil"
+	"net"
+	"testing"
+
+	"github.com/docker/docker/api/types"
+	"github.com/hashicorp/go-hclog"
+	clients "github.com/shipyard-run/shipyard/pkg/clients/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func setupShellMocks() (*DockerTasks, *clients.MockDocker) {
+	md := &clients.MockDocker{}
+	md.On("ContainerExecCreate", mock.Anything, mock.Anything, mock.Anything).Return(types.IDResponse{ID: "123"}, nil)
+	md.On("ContainerExecAttach", mock.Anything, "123", mock.Anything).Return(
+		types.HijackedResponse{
+			Conn: &net.TCPConn{},
+			Reader: bufio.NewReader(
+				bytes.NewReader([]byte("log output")),
+			),
+		}, nil)
+	md.On("ContainerExecInspect", mock.Anything, mock.Anything).Return(types.ContainerExecInspect{ExitCode: 0}, nil)
+
+	p := NewDockerTasks(md, hclog.NewNullLogger())
+
+	return p, md
+}
+
+func TestCreateShellCreatesExec(t *testing.T) {
+	p, md := setupShellMocks()
+	in := ioutil.NopCloser(bytes.NewReader([]byte("abc")))
+	out := ioutil.Discard
+	errW := ioutil.Discard
+
+	err := p.CreateShell("abc", []string{"sh"}, in, out, errW)
+	assert.NoError(t, err)
+
+	md.AssertCalled(t, "ContainerExecCreate", mock.Anything, "abc", mock.Anything)
+
+}
