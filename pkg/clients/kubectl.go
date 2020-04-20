6a7c3cbded5d58ca8405e0fffcdae82bcf1f6a6b
Nic Jackson <jackson.nic@gmail.com>
2020-04-20 18:23:47 +0100

    Add pod status diagnostic

--- pkg/clients/kubectl.go
+++ pkg/clients/kubectl.go
@@ -179,7 +179,7 @@ func (k *KubernetesImpl) healthCheckSingle(selector string, timeout time.Duratio
 		for _, pod := range pl.Items {
 			if pod.Status.Phase != "Running" {
 				allRunning = false
-				k.l.Debug("Pod not running", "pod", pod.Name, "namespace", pod.Namespace)
+				k.l.Debug("Pod not running", "pod", pod.Name, "namespace", pod.Namespace, "status", pod.Status.Phase)
 				break
 			}
 
@@ -187,8 +187,7 @@ func (k *KubernetesImpl) healthCheckSingle(selector string, timeout time.Duratio
 			for _, s := range pod.Status.ContainerStatuses {
 				if !s.Ready {
 					allRunning = false
-					k.l.Debug("Pod not ready", "pod", pod.Name, "namespace", pod.Namespace)
-					break
+					k.l.Debug("Pod not ready", "pod", pod.Name, "namespace", pod.Namespace, "container", s.Name, "status", s.State.String())
 				}
 			}
 		}
