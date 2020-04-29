a3dbcfce82d7d2dba045432d2184c10c29e85a9c
Nic Jackson <jackson.nic@gmail.com>
2020-04-29 09:23:37 +0100

    Comment out Script parameter which is not currently implemented

--- pkg/config/exec_remote.go
+++ pkg/config/exec_remote.go
@@ -16,9 +16,9 @@ type ExecRemote struct {
 	Target string `hcl:"target,optional" json:"target,omitempty"` // Attach to a running target and exec
 
 	// Either Script or Command must be specified
-	Script    string   `hcl:"script,optional" json:"script,omitempty"` // Path to a script to execute
-	Command   string   `hcl:"cmd,optional" json:"cmd,omitempty"`       // Command to execute
-	Arguments []string `hcl:"args,optional" json:"args,omitempty"`     // only used when combined with Command
+	//Script    string   `hcl:"script,optional" json:"script,omitempty"` // Path to a script to execute
+	Command   string   `hcl:"cmd,optional" json:"cmd,omitempty"`   // Command to execute
+	Arguments []string `hcl:"args,optional" json:"args,omitempty"` // only used when combined with Command
 
 	Volumes     []Volume `hcl:"volume,block" json:"volumes,omitempty"` // Volumes to mount to container
 	Environment []KV     `hcl:"env,block" json:"env,omitempty"`        // Environment varialbes to set
