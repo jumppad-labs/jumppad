3dc8433e419e1f6b31b89219fc0239b84d82c201
Nic Jackson <jackson.nic@gmail.com>
2020-08-26 19:53:33 +0100

    Add remote k8s example

---
+++ examples/single_k3s_cluster/connector_config/script.sh
@@ -0,0 +1,52 @@
+# Expose Kubernetes service to local machine
+# k8s_ingress "k8s_web" {
+#   cluster = "k8s_cluster.k3s"
+#   service  = "connector-service-web"
+# 
+#   port {
+#     local  = 9090
+#     host   = 12000
+#   }
+
+#   port {
+#     local  = 9091
+#     host   = 12001
+#   }
+# }
+
+curl localhost:9091/expose -d \
+  '{
+    "name":"k8s_web", 
+    "local_port": 12000, 
+    "remote_port": 9090, 
+    "remote_server_addr": "server.k3s.k8s_cluster.shipyard.run:30000", 
+    "service_addr": "connector-service-web:9090",
+    "type": "remote"
+  }'
+
+# Expose local service to kubernetes
+# local_service "local_fake" {
+#   cluster = "k8s_cluster.k3s"
+#   host = "172.31.92.175:19000
+# 
+#   port {
+#     local  = 19090
+#     remote = 13000
+#   }
+
+#   port {
+#     local  = 19091
+#     remote = 13001
+#   }
+#  
+# }
+
+curl localhost:9091/expose -d \
+  '{
+    "name":"local_fake", 
+    "local_port": 19090, 
+    "remote_port": 13000, 
+    "remote_server_addr": "server.k3s.k8s_cluster.shipyard.run:30000", 
+    "service_addr": "172.31.92.175:19090",
+    "type": "local"
+  }'
--- examples/single_k3s_cluster/k8s.hcl
+++ examples/single_k3s_cluster/k8s.hcl
@@ -12,3 +12,11 @@ k8s_cluster "k3s" {
     name = "consul:1.6.1"
   }
 }
+
+k8s_config "connector" {
+  cluster = "k8s_cluster.k3s"
+
+  paths = ["./connector_config"]
+
+  wait_until_ready = false
+}
