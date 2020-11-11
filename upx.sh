1fd1c8f74f820a718615aefb44754ed30d587a61
Nic Jackson <jackson.nic@gmail.com>
2020-11-11 09:49:43 +0000

    Move UPX install to shell script

--- upx.sh
+++ upx.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-upx ./dist/shipyard_linux_amd64/shipyard
-upx ./dist/shipyard_linux_arm64/shipyard
-upx ./dist/shipyard_linux_armv7/shipyard
-upx ./dist/shipyard_windows_amd64/shipyard
-upx ./dist/shipyard_darwin_amd64/shipyard
+# Install UPX
+wget https://github.com/upx/upx/releases/download/v3.96/upx-3.96-amd64_linux.tar.xz
+tar -xf upx-3.96-amd64_linux.tar.xz
+
+# Compress files
+./upx ./dist/shipyard_linux_amd64/shipyard
+./upx ./dist/shipyard_linux_arm64/shipyard
+./upx ./dist/shipyard_linux_armv7/shipyard
+./upx ./dist/shipyard_windows_amd64/shipyard
+./upx ./dist/shipyard_darwin_amd64/shipyard
+
+rm -rf upx-3.96-amd64_linux
+rm -rf ./upx
+rm -rf upx-3.96-amd64_linux.tar.xz
