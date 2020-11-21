ac205bac388499d307ad4416cd28df740a9a94ec
Nic Jackson <jackson.nic@gmail.com>
2020-11-21 14:16:37 +0000

    Updated compress binary command

--- upx.sh
+++ upx.sh
@@ -5,12 +5,8 @@ wget https://github.com/upx/upx/releases/download/v3.96/upx-3.96-amd64_linux.tar
 tar -xf upx-3.96-amd64_linux.tar.xz
 
 # Compress files
-./upx ./dist/shipyard_linux_amd64/shipyard
-./upx ./dist/shipyard_linux_arm64/shipyard
-./upx ./dist/shipyard_linux_armv7/shipyard
-./upx ./dist/shipyard_windows_amd64/shipyard
-./upx ./dist/shipyard_darwin_amd64/shipyard
+./upx-3.96-amd64_linux/upx $1
 
 rm -rf upx-3.96-amd64_linux
-rm -rf ./upx
 rm -rf upx-3.96-amd64_linux.tar.xz
+rm -rf upx-3.96-amd64_linux.tar.xz.*
