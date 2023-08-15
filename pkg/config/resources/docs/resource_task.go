a9d76b4c10480f61836ccc8119b10786fbb16a3c
Erik Veld <eveld@hashicorp.com>
2023-08-15 16:54:09 +0200

    Implement new lifecycle hooks

--- pkg/config/resources/docs/resource_task.go
+++ pkg/config/resources/docs/resource_task.go
@@ -16,20 +16,112 @@ type Task struct {
 }
 
 type Condition struct {
-	Name             string `hcl:"id,label" json:"id"`
-	Description      string `hcl:"description" json:"description"`
-	Check            string `hcl:"check" json:"check"`
-	Solve            string `hcl:"solve,optional" json:"solve,omitempty"`
-	FailureMessage   string `hcl:"failure_message" json:"failure_message,omitempty"`
-	SuccessMessage   string `hcl:"success_message,optional" json:"success_message,omitempty"`
-	Target           string `hcl:"target,optional" json:"target,omitempty"`
-	User             string `hcl:"user,optional" json:"user,omitempty"`
-	WorkingDirectory string `hcl:"working_directory,optional" json:"working_directory,omitempty"`
-	Status           string `hcl:"status,optional" json:"status"`
+	Name        string       `hcl:"id,label" json:"id"`
+	Description string       `hcl:"description" json:"description"`
+	Checks      []Validation `hcl:"check,block" json:"checks"`
+	Solves      []Validation `hcl:"solve,block" json:"solves,omitempty"`
+	Setups      []Validation `hcl:"setup,block" json:"setups,omitempty"`
+	Cleanups    []Validation `hcl:"cleanup,block" json:"cleanups,omitempty"`
+	Status      string       `hcl:"status,optional" json:"status"`
+}
+
+type Validation struct {
+	Script         string `hcl:"script" json:"script"`
+	FailureMessage string `hcl:"failure_message,optional" json:"failure_message,omitempty"`
+	SuccessMessage string `hcl:"success_message,optional" json:"success_message,omitempty"`
+	Config         `hcl:",remain"`
 }
 
 type Config struct {
+	Timeout          int    `hcl:"timeout,optional" json:"timeout"`
 	Target           string `hcl:"target,optional" json:"target,omitempty"`
 	User             string `hcl:"user,optional" json:"user,omitempty"`
+	Group            string `hcl:"group,optional" json:"group,omitempty"`
 	WorkingDirectory string `hcl:"working_directory,optional" json:"working_directory,omitempty"`
+}
+
+func (t *Task) Process() error {
+	// Set defaults
+	if t.Config.Timeout == 0 {
+		t.Config.Timeout = 30
+	}
+
+	if t.Config.User == "" {
+		t.Config.User = "root"
+	}
+
+	if t.Config.WorkingDirectory == "" {
+		t.Config.WorkingDirectory = "/"
+	}
+
+	// set overrides
+	for i, condition := range t.Conditions {
+		for j, check := range condition.Checks {
+			if check.Timeout == 0 {
+				t.Conditions[i].Checks[j].Timeout = t.Config.Timeout
+			}
+
+			if check.Target == "" {
+				t.Conditions[i].Checks[j].Target = t.Config.Target
+			}
+
+			if check.User == "" {
+				t.Conditions[i].Checks[j].User = t.Config.User
+			}
+
+			if check.Group == "" {
+				t.Conditions[i].Checks[j].Group = t.Config.Group
+			}
+
+			if check.WorkingDirectory == "" {
+				t.Conditions[i].Checks[j].WorkingDirectory = t.Config.WorkingDirectory
+			}
+		}
+
+		for j, solve := range condition.Solves {
+			if solve.Timeout == 0 {
+				t.Conditions[i].Solves[j].Timeout = t.Config.Timeout
+			}
+
+			if solve.Target == "" {
+				t.Conditions[i].Solves[j].Target = t.Config.Target
+			}
+
+			if solve.User == "" {
+				t.Conditions[i].Solves[j].User = t.Config.User
+			}
+
+			if solve.Group == "" {
+				t.Conditions[i].Solves[j].Group = t.Config.Group
+			}
+
+			if solve.WorkingDirectory == "" {
+				t.Conditions[i].Solves[j].WorkingDirectory = t.Config.WorkingDirectory
+			}
+		}
+
+		for j, setup := range condition.Setups {
+			if setup.Timeout == 0 {
+				t.Conditions[i].Setups[j].Timeout = t.Config.Timeout
+			}
+
+			if setup.Target == "" {
+				t.Conditions[i].Setups[j].Target = t.Config.Target
+			}
+
+			if setup.User == "" {
+				t.Conditions[i].Setups[j].User = t.Config.User
+			}
+
+			if setup.Group == "" {
+				t.Conditions[i].Setups[j].Group = t.Config.Group
+			}
+
+			if setup.WorkingDirectory == "" {
+				t.Conditions[i].Setups[j].WorkingDirectory = t.Config.WorkingDirectory
+			}
+		}
+	}
+
+	return nil
 }
--- pkg/server/validation.go
+++ pkg/server/validation.go
@@ -3,9 +3,11 @@ package server
 import (
 	"bytes"
 	"encoding/json"
+	"io"
 	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
 	"github.com/jumppad-labs/hclconfig/types"
 	"github.com/jumppad-labs/jumppad/pkg/clients/container"
 	"github.com/jumppad-labs/jumppad/pkg/clients/images"
@@ -24,28 +26,93 @@ type ValidationResponse struct {
 
 type ValidationRequest struct {
 	Action    string `json:"action"`
-	Timeout   int    `json:"timeout"`
 	Task      string `json:"task"`
 	Condition string `json:"condition"`
 	WorkDir   string `json:"work_dir"`
 	User      string `json:"user"`
+	Group     string `json:"group"`
 	Target    string `json:"target"`
 }
 
+type validationRequest struct {
+	ContinueOnFail bool `json:"continue_on_fail"`
+}
+
+type validationResponse struct {
+	ID         string                `json:"id"`
+	Conditions []validationCondition `json:"conditions"`
+	Status     string                `json:"status"`
+}
+
+type validationCondition struct {
+	ID       string   `json:"id"`
+	Messages []string `json:"messages"`
+	Status   string   `json:"status"`
+}
+
+func getTarget(id string) (string, error) {
+	fqrn, err := types.ParseFQRN(id)
+	if err != nil {
+		return "", err
+	}
+
+	target := utils.FQDN(fqrn.Resource, fqrn.Module, fqrn.Type)
+	return target, nil
+}
+
+func (a *API) executeScript(target string, script string, workdir string, user string, group string, timeout int) (int, string) {
+	dc, err := container.NewDocker()
+	if err != nil {
+		return 254, err.Error()
+	}
+
+	il := images.NewImageFileLog(utils.ImageCacheLog())
+	tz := &tar.TarGz{}
+	ct := container.NewDockerTasks(dc, il, tz, a.log)
+
+	fqdn, err := getTarget(target)
+	if err != nil {
+		return 254, err.Error()
+	}
+
+	id, err := ct.FindContainerIDs(fqdn)
+	if err != nil || len(id) == 0 {
+		return 254, err.Error()
+	}
+
+	env := os.Environ()
+	env = append(env, "TERM=xterm")
+
+	output := bytes.NewBufferString("")
+
+	var message string
+	exitCode, err := ct.ExecuteScript(id[0], script, env, workdir, user, group, timeout, output)
+	if err != nil {
+		if exitCode != 1 {
+			a.log.Error("exec failed", "error", err.Error(), "message", message)
+			message = err.Error()
+		}
+	}
+
+	return exitCode, message
+}
+
 func (a *API) validation(w http.ResponseWriter, r *http.Request) {
-	req := ValidationRequest{
-		Action:  "check",
-		WorkDir: "/",
-		User:    "root",
-		Timeout: 10,
+	taskID := chi.URLParam(r, "task")
+	action := chi.URLParam(r, "action")
+
+	req := validationRequest{
+		ContinueOnFail: false,
 	}
 
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
-		a.log.Error("could not decode validation request", "error", err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(err.Error())
-		return
+		if err != io.EOF {
+			a.log.Error("could not decode validation request", "error", err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(err.Error())
+			return
+		}
 	}
 
 	state, err := config.LoadState()
@@ -56,7 +123,7 @@ func (a *API) validation(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	res, err := state.FindResource(req.Task)
+	res, err := state.FindResource(taskID)
 	if err != nil {
 		a.log.Error("could not find task", "error", err.Error())
 		w.WriteHeader(http.StatusInternalServerError)
@@ -66,86 +133,155 @@ func (a *API) validation(w http.ResponseWriter, r *http.Request) {
 
 	task := res.(*docs.Task)
 
-	var target string
-	var message string
-	var script string
+	var conditions []validationCondition
 
-	for _, c := range task.Conditions {
-		if c.Name == req.Condition {
-			defaultTarget := task.Config.Target
-			if c.Target != "" {
-				defaultTarget = c.Target
-			}
+	completed := 0
+	status := "failed"
+	for _, condition := range task.Conditions {
+		var validations []docs.Validation
 
-			fqrn, err := types.ParseFQRN(defaultTarget)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				json.NewEncoder(w).Encode(err.Error())
-				return
-			}
-
-			target = utils.FQDN(fqrn.Resource, fqrn.Module, fqrn.Type)
-
-			message = c.FailureMessage
-
-			if req.Action == "solve" {
-				script = c.Solve
-			} else {
-				script = c.Check
-			}
-
-			break
+		switch action {
+		case "check":
+			validations = condition.Checks
+		case "solve":
+			validations = condition.Solves
+		case "setup":
+			validations = condition.Setups
+		case "cleanup":
+			validations = condition.Cleanups
 		}
-	}
 
-	if script == "" {
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(ValidationResponse{
-			Task:      req.Task,
-			Condition: req.Condition,
-			ExitCode:  0,
+		status := ""
+		messages := []string{}
+
+		for _, validation := range validations {
+			if validation.Script == "" {
+				continue
+			}
+
+			exitCode, message := a.executeScript(validation.Target, validation.Script, validation.WorkingDirectory, validation.User, validation.Group, validation.Timeout)
+			if exitCode != 0 {
+				status = "failed"
+				if exitCode == 1 {
+					message = validation.FailureMessage
+				}
+
+				messages = append(messages, message)
+			}
+		}
+
+		if status == "" {
+			status = "completed"
+			completed++
+		}
+
+		conditions = append(conditions, validationCondition{
+			ID:       condition.Name,
+			Messages: messages,
+			Status:   status,
 		})
-		return
 	}
 
-	dc, err := container.NewDocker()
-	if err != nil {
-		a.log.Error("Could not create docker client", "error", err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(err.Error())
-		return
-	}
-
-	il := images.NewImageFileLog(utils.ImageCacheLog())
-	tz := &tar.TarGz{}
-	ct := container.NewDockerTasks(dc, il, tz, a.log)
-
-	id, err := ct.FindContainerIDs(target)
-	if err != nil || len(id) == 0 {
-		a.log.Error("Could not find container for target", "target", target)
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode("Could not find container for target")
-		return
-	}
-
-	env := os.Environ()
-	env = append(env, "TERM=xterm")
-
-	output := bytes.NewBufferString("")
-
-	exitCode, err := ct.ExecuteScript(id[0], script, env, req.WorkDir, req.User, "", req.Timeout, output)
-	if err != nil {
-		if exitCode != 1 {
-			a.log.Error("exec failed", "error", err.Error(), "message", message)
-			message = err.Error()
-		}
+	if len(conditions) == completed {
+		status = "completed"
 	}
 
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(ValidationResponse{
-		Task:      req.Task,
-		Condition: req.Condition,
-		ExitCode:  exitCode,
-		Message:   message,
+	json.NewEncoder(w).Encode(validationResponse{
+		ID:         task.ID,
+		Conditions: conditions,
+		Status:     status,
 	})
+
+	// var target string
+	// var message string
+	// var script string
+	// var timeout int
+	// var user string
+	// var group string
+	// var workdir string
+
+	// for _, c := range task.Conditions {
+	// 	if c.Name == req.Condition {
+	// 		defaultTarget := task.Config.Target
+	// 		if c.Target != "" {
+	// 			defaultTarget = c.Target
+	// 		}
+
+	// 		fqrn, err := types.ParseFQRN(defaultTarget)
+	// 		if err != nil {
+	// 			w.WriteHeader(http.StatusInternalServerError)
+	// 			json.NewEncoder(w).Encode(err.Error())
+	// 			return
+	// 		}
+
+	// 		target = utils.FQDN(fqrn.Resource, fqrn.Module, fqrn.Type)
+
+	// 		message = c.FailureMessage
+	// 		user = c.User
+	// 		group = c.Group
+	// 		workdir = c.WorkingDirectory
+
+	// 		if req.Action == "solve" {
+	// 			script = c.Solve
+	// 			timeout = c.SolveTimeout
+	// 		} else {
+	// 			script = c.Check
+	// 			timeout = c.CheckTimeout
+	// 		}
+
+	// 		break
+	// 	}
+	// }
+
+	// if script == "" {
+	// 	w.WriteHeader(http.StatusOK)
+	// 	json.NewEncoder(w).Encode(ValidationResponse{
+	// 		Task:      req.Task,
+	// 		Condition: req.Condition,
+	// 		ExitCode:  0,
+	// 	})
+	// 	return
+	// }
+
+	// dc, err := container.NewDocker()
+	// if err != nil {
+	// 	a.log.Error("Could not create docker client", "error", err.Error())
+	// 	w.WriteHeader(http.StatusInternalServerError)
+	// 	json.NewEncoder(w).Encode(err.Error())
+	// 	return
+	// }
+
+	// il := images.NewImageFileLog(utils.ImageCacheLog())
+	// tz := &tar.TarGz{}
+	// ct := container.NewDockerTasks(dc, il, tz, a.log)
+
+	// id, err := ct.FindContainerIDs(target)
+	// if err != nil || len(id) == 0 {
+	// 	a.log.Error("Could not find container for target", "target", target)
+	// 	w.WriteHeader(http.StatusInternalServerError)
+	// 	json.NewEncoder(w).Encode("Could not find container for target")
+	// 	return
+	// }
+
+	// env := os.Environ()
+	// env = append(env, "TERM=xterm")
+
+	// output := bytes.NewBufferString("")
+
+	// exitCode, err := ct.ExecuteScript(id[0], script, env, workdir, user, group, timeout, output)
+	// if err != nil {
+	// 	if exitCode != 1 {
+	// 		a.log.Error("exec failed", "error", err.Error(), "message", message)
+	// 		message = err.Error()
+	// 	}
+	// }
+
+	// w.WriteHeader(http.StatusOK)
+	// json.NewEncoder(w).Encode(ValidationResponse{
+	// 	Task:      req.Task,
+	// 	Condition: req.Condition,
+	// 	ExitCode:  exitCode,
+	// 	Message:   message,
+	// })
 }
